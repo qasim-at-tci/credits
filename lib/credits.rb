d0fa3fd20c5c70eb003cc1ffd4c00f11a210b8f5
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2022-07-16 01:02:54 +0500

    copped [skip-ci]

--- lib/credits.rb
+++ lib/credits.rb
@@ -1,12 +1,14 @@
-require "credits/version"
-require "credits/credits"
+# frozen_string_literal: true
+
+require 'credits/version'
+require 'credits/credits'
 require 'thor'
 
 module Credits
   class Error < StandardError; end
-  
+
   class CLI < Thor
-    desc "remaining [token][username]", "Gives back remaining credits of Travis account"
+    desc 'remaining [token][username]', 'Gives back remaining credits of Travis account'
     def remaining(token, username)
       Credits.request_remaining(token, username)
     end
--- lib/credits/credits.rb
+++ lib/credits/credits.rb
@@ -1,52 +1,53 @@
+# frozen_string_literal: true
+
 require 'httparty'
 
 module Credits
-	def self.request_remaining token, username
-		@owner = nil
-		@url = "https://api.travis-ci.com/v2_subscriptions"
+  def self.request_remaining(token, username)
+    @owner = nil
+    @url = 'https://api.travis-ci.com/v2_subscriptions'
 
-		headers = { 
-						"Authorization"  => "token #{token}",
-						"Content-Type" => "application/json",
-						"Accept" => "application/json",
-						"Travis-API-Version" => "3",
-						"path" => "/v2_subscriptions"
-		}
-		response = HTTParty.get(
-		@url,
-		:query => {format: :json},
-		:headers => headers
-		)
+    headers = {
+      'Authorization' => "token #{token}",
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json',
+      'Travis-API-Version' => '3',
+      'path' => '/v2_subscriptions'
+    }
+    response = HTTParty.get(
+      @url,
+      query: { format: :json },
+      headers: headers
+    )
 
-		if (response.code == 200)
-			response['v2_subscriptions'].each do |sub|
-				if sub['owner']['@type'] == "user" && sub['owner']['login'] == username
-					@owner = username
-				elsif sub['owner']['@type'] == "organization" && sub['owner']['login'] == username
-					@owner = username
-				end
-			end
-		else
-		end
+    if response.code == 200
+      response['v2_subscriptions'].each do |sub|
+        if sub['owner']['@type'] == 'user' && sub['owner']['login'] == username
+          @owner = username
+        elsif sub['owner']['@type'] == 'organization' && sub['owner']['login'] == username
+          @owner = username
+        end
+      end
+    end
 
-		if (response.code == 200 && @owner != nil)
-			response['v2_subscriptions'].each do |sub|
-				if sub['owner']['@type'] == "user" && sub['owner']['login'] == username
-					owner = sub['owner']['login']
-					remaining_credits = sub['addons'][0]['current_usage']['remaining']
+    if response.code == 200 && !@owner.nil?
+      response['v2_subscriptions'].each do |sub|
+        if sub['owner']['@type'] == 'user' && sub['owner']['login'] == username
+          owner = sub['owner']['login']
+          remaining_credits = sub['addons'][0]['current_usage']['remaining']
 
-					print "User: #{owner}\n"
-					print "Remaining Travis CI Credits: #{remaining_credits}\n"
-				elsif sub['owner']['@type'] == "organization" && sub['owner']['login'] == username
-					owner = sub['owner']['login']
-					remaining_credits = sub['addons'][0]['current_usage']['remaining']
+          print "User: #{owner}\n"
+          print "Remaining Travis CI Credits: #{remaining_credits}\n"
+        elsif sub['owner']['@type'] == 'organization' && sub['owner']['login'] == username
+          owner = sub['owner']['login']
+          remaining_credits = sub['addons'][0]['current_usage']['remaining']
 
-					print "User: #{owner}\n"
-					print "Remaining Travis CI Credits: #{remaining_credits}\n"
-				end
-			end
-		else
-			print "Wrong token or username"
-		end
-	end
+          print "User: #{owner}\n"
+          print "Remaining Travis CI Credits: #{remaining_credits}\n"
+        end
+      end
+    else
+      print 'Wrong token or username'
+    end
+  end
 end
